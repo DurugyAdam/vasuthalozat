a4cd29ee4f48f6dbc75fd58ba93b9fb969da4479
DurugyAdam <adam.durugy@gmail.com>
2022-02-04 16:28:55 +0100

    .

---
+++ WpfApp1/common library/repository/UserDbContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using common_library.model;
+//using Microsoft.EntityFrameworkCore;
+
+namespace common_library.repository
+{
+    class UserDbContext // : DbContext
+    {
+        private static UserDbContext instance = null;
+
+        // public DbSet<User> Users { get; set; }
+
+        public static UserDbContext Instance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    instance = new UserDbContext();
+                }
+                return instance;
+            }
+        }
+
+        private UserDbContext()
+        {
+            //Database.EnsureCreated();
+        }
+
+        /*protected override void OnConfiguration(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }*/
+    }
+}
+
